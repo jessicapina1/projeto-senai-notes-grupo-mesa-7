c5dff02d73cb87182693caa06ed4770e9ac53432
Cesar Augusto <cesarprofissional22@gmail.com>
2025-09-24 20:16:02 -0300

    Criando telas

---
+++ senai-notes-angular/src/app/all-notes/all-notes.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AllNotes } from './all-notes';
+
+describe('AllNotes', () => {
+  let component: AllNotes;
+  let fixture: ComponentFixture<AllNotes>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AllNotes]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AllNotes);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ senai-notes-angular/src/app/all-notes/all-notes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-notes',
+  imports: [],
+  templateUrl: './all-notes.html',
+  styleUrl: './all-notes.css'
+})
+export class AllNotes {
+
+}
