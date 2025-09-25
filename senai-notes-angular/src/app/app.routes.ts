788a1f591078be67257337df3dd1871bffea118c
Cesar Augusto <cesarprofissional22@gmail.com>
2025-09-24 21:04:41 -0300

    Rotas finalizados

--- senai-notes-angular/src/app/app.routes.ts
+++ senai-notes-angular/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginScreen } from './user-module/login-screen/login-screen';
+import { NewUserScreen } from './user-module/new-user-screen/new-user-screen';
+import { AllNotes } from './all-notes/all-notes';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    
+    {
+        path: "login",
+        loadComponent: () => LoginScreen
+    },
+    {
+        path: "new-user",
+        loadComponent: () =>NewUserScreen
+        
+    },
+    {
+        path: "notes",
+        loadComponent: () => AllNotes
+}
+];
